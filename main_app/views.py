4fe486de591362e4d7b127ba027ad9acef8d01c7
aikinjess <aikinjess@gmail.com>
2021-05-04 16:48:34 -0400

    about template

--- main_app/views.py
+++ main_app/views.py
@@ -6,6 +6,6 @@ from django.http import HttpResponse
 
 def home(request):
   return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
-  
+
 def about(request):
-    return HttpResponse('<h1>About</h1>')
+  return render(request, 'about.html')
